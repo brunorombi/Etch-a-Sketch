ed5934ae2214fca8ad0fd5c5306f8b29d760e716
Bruno Rombi <brunorombi@yahoo.com>
2025-09-17 20:26:51 -0300

    Add hover effects to blocks

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const grid = document.querySelector(".grid");
 
+
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
-        let square = document.createElement('div');
-        square.classList.add('block');
-        grid.append(square);
+        let block = document.createElement('div');
+        block.classList.add('block');
+        grid.append(block);
     }
 }
 
+const blocks = document.querySelectorAll(".block")
+
+blocks.forEach(function (block) {
+    block.addEventListener('mouseenter', () => {
+        block.classList.add('block-color')
+    })
+})
